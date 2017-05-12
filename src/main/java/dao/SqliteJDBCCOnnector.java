c725b01e1f2ae2ad7b644c83a5288d1a0cff96cd
Jakub <kubawl.91@gmail.com>
2017-05-12 19:40:27 +0200

    add methods : connection , and create table to connector

--- src/main/java/dao/SqliteJDBCCOnnector.java
+++ src/main/java/dao/SqliteJDBCCOnnector.java
@@ -1,7 +1,35 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by jakub on 12/05/17.
  */
 public class SqliteJDBCCOnnector {
+
+    public static Connection connection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+        } catch (SQLException e) {
+            System.out.println("Connection to database failed.");
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void createTables() throws SQLException {
+        Connection connection = connection();
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS \"events\" (\n" +
+                "\t`id`\tINTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "\t`name`\tTEXT,\n" +
+                "\t`description`\tTEXT,\n" +
+                "\t`date`\tTEXT,\n" +
+                "\t`category`\tTEXT,\n" +
+                ")");
+    }
 }
