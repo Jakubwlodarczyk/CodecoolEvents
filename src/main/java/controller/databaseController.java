cfe11f0e34abafd9c5da0f386cad6b83b786e1a8
Jakub <kubawl.91@gmail.com>
2017-05-15 18:05:59 +0200

    add method to create new database file

--- src/main/java/controller/databaseController.java
+++ src/main/java/controller/databaseController.java
@@ -1,7 +1,26 @@
 package controller;
 
+import dao.EventDao;
+
+import java.sql.SQLException;
+
+import static spark.Spark.exception;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
 /**
  * Created by jakub on 15/05/17.
  */
 public class databaseController {
+
+    public static void createTable(String[] args) {
+        if(args.length > 0 && args[0].equals("--create-tables")) {
+            try {
+                EventDao.createTables();
+            } catch (SQLException e) {
+                System.out.println("Cannot create tables in db.");
+                System.out.println(e);
+            }
+        }
+    }
 }
