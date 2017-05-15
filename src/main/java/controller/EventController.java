53a26f22f2ba79becbc09446bfceb396801de49e
Jakub <kubawl.91@gmail.com>
2017-05-15 18:33:00 +0200

    change unstatic methods call to static methods call , and delete unused imports

--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -1,35 +1,26 @@
 package controller;
 
-import com.sun.org.apache.regexp.internal.RE;
 import dao.EventDao;
-import model.Event;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
-
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by rafalstepien on 28/04/2017.
- */
 public class EventController {
 
     public static ModelAndView renderEvents(Request req, Response res) {
-        EventDao eventDao = new EventDao();
         Map params = new HashMap<>();
-        params.put("eventContainer", eventDao.getAll());
+        params.put("eventContainer", EventDao.getAll());
         return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderEventsInfo(Request req, Response res) {
-        EventDao eventDao = new EventDao();
         Map params = new HashMap<>();
         String stringId = req.queryParams("event-id");
         Integer integerId = Integer.parseInt(stringId);
-        params.put("event", eventDao.getById(integerId));
-        Event event = eventDao.getById(integerId);
+        params.put("event", EventDao.getById(integerId));
         return new ModelAndView(params,"product/eventInfo");
     }
 
@@ -45,11 +36,10 @@ public class EventController {
         String eventTime = req.queryParams("event-time");
         String eventCategory = req.queryParams("event-category");
         String fullDate = eventDate + " " +eventTime;
-        EventDao eventDao = new EventDao();
-        eventDao.addEventToDatabase(eventName, eventDescription, fullDate, eventCategory);
+        EventDao.addEventToDatabase(eventName, eventDescription, fullDate, eventCategory);
 
         Map params = new HashMap<>();
-        params.put("eventContainer", eventDao.getAll());
+        params.put("eventContainer", EventDao.getAll());
         return new ModelAndView(params, "product/index");
     }
 }
