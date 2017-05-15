352ecab499ad9050a424f2e3f9468f2b2d53200f
Jakub <kubawl.91@gmail.com>
2017-05-15 18:03:33 +0200

    remove table creator from main, and delete example path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,25 @@
 import controller.EventController;
-import dao.EventDao;
 import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
-
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-
 import  static spark.Spark.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        if(args.length > 0 && args[0].equals("--create-tables")) {
-            try {
-                EventDao.createTables();
-            } catch (SQLException e) {
-                System.out.println("Cannot create tables in db.");
-                System.out.println(e);
-            }
-        }
 
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
 
-        // Always start with more specific routes
-        get("/hello", (req, res) -> "Hello World");
-
-        // Always add generic routes to the end
         get("/", EventController::renderEvents, new ThymeleafTemplateEngine());
-        // Equivalent with above
-        get("/index", (Request req, Response res) -> {
-            return new ThymeleafTemplateEngine().render( EventController.renderEvents(req, res) );
-        });
 
         get("/event-info", EventController::renderEventsInfo, new ThymeleafTemplateEngine() );
 
         get("/add-event", EventController::renderAddForm, new ThymeleafTemplateEngine() );
 
-        post("/add-event", EventController::addNewEvent, new ThymeleafTemplateEngine());
+        post("/add-event", EventController::addNewEvent, new ThymeleafTemplateEngine() );
     }
-
-
+    
 }
---
+++ src/main/java/controller/databaseController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by jakub on 15/05/17.
+ */
+public class databaseController {
+}
